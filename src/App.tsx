0a896993922b5cbdf78c3963f6f817d02692565d
hwanga <hwanga555@gmail.com>
2025-12-09 10:27:15 +0900

    PR 테스트용 커밋1

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,23 @@
 import { Button } from "./components/ui/button";
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "./components/ui/accordion";
 
 function App() {
   return (
     <>
       <div>APP 화면</div>
       <Button>임시 버튼 테스트</Button>
+
+      <Accordion type="single" collapsible>
+        <AccordionItem value="item-1">
+          <AccordionTrigger>아코디언 테스트</AccordionTrigger>
+          <AccordionContent>열었다 닫았다 가능해요</AccordionContent>
+        </AccordionItem>
+      </Accordion>
     </>
   );
 }
