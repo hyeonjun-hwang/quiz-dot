923e4f438209160cae83a8b0c69454fd93e29c89
hwanga <hwanga555@gmail.com>
2025-12-10 11:21:18 +0900

    임시 테스트

--- src/App.tsx
+++ src/App.tsx
@@ -9,13 +9,13 @@ import {
 function App() {
   return (
     <>
+      {/* 테스트용 임시 코드 */}
       <div>APP 화면</div>
       <p>코드 수정1</p>
       <p>코드 수정2</p>
       <p>코드 수정3</p>
       <Button>임시 버튼 테스트</Button>
-
-      <Accordion type="single" collapsible>
+      <Accordion type="single" collapsible className="w-fit">
         <AccordionItem value="item-1">
           <AccordionTrigger>아코디언 테스트</AccordionTrigger>
           <AccordionContent>열었다 닫았다 가능해요</AccordionContent>
