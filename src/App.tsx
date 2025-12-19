f47668fae7b8829188734d64c3afd7f3928e3249
SSOL-2 <dkrxhvjr@gmail.com>
2025-12-19 17:41:47 +0900

    [feat]로그인 UI 및 일반 로그인

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,12 @@
+import { Toaster } from "sonner";
+
 function App() {
-  return <div>테스트입니다</div>;
+  return (
+    <div>
+      테스트입니다
+      <Toaster position="top-center" richColors closeButton />
+    </div>
+  );
 }
 
 export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,9 @@ import { BrowserRouter, Routes, Route } from "react-router";
 import { RootLayout } from "./pages/RootLayout.tsx";
 import "./index.css";
 import App from "./App.tsx";
+import Signin from "./pages/auth/sign-in.tsx";
+import Signup from "./pages/auth/sign-up.tsx";
+import { Toaster } from "sonner";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
@@ -12,8 +15,13 @@ createRoot(document.getElementById("root")!).render(
         <Route element={<RootLayout />}>
           {/* ROOT */}
           <Route path="/" element={<App />} />
+          {/* AUTH */}
+          <Route path="/sign-in" element={<Signin />} />
+          <Route path="/sign-up" element={<Signup />} />
         </Route>
       </Routes>
+      {/* 어떤 페이지에서든 toast 부르기 */}
+      <Toaster position="top-center" richColors closeButton />
     </BrowserRouter>
   </StrictMode>
 );
